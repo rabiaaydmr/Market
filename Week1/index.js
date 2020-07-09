c6c554abe609bb6d4ac5417ee745832b91f016b2
rabiaaydmr <rabiaaydemir@sabanciuniv.edu>
2020-07-09 13:26:41 +0300

    Fix Code

--- Week1/index.js
+++ Week1/index.js
@@ -1,53 +1,52 @@
-const Market = class {
-    constructor(name, address) {
-        this.name = name
-        this.address = address
-        this.order1 = []
-    }
-    prepare(product) {
-        console.log("Prepared the " + product.name + " !")
-        this.order1.push(product)
-    }
- 
-    printOrder(order1)
-    {
-    this.order1.forEach(printName)
-    }
+class Market {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+    this.order1 = [];
+  }
+  prepare(product) {
+    console.log("Prepared the " + product.name + " !");
+    this.order1.push(product);
+  }
 
+  printOrder(order1) {
+    this.order1.forEach(printName);
+  }
 }
-const FastFood = class {
-    constructor(name) {
-        this.name = name
-    }
-
+class FastFood {
+  constructor(name) {
+    this.name = name;
+  }
 }
-const Customer = class {
-    constructor(name, address) {
-        this.name = name
-        this.address = address
+class Customer {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
 
-    }
-
-    order(product, market) {
-        market.prepare(product)
-    }
-
+  order(product, market) {
+    market.prepare(product);
+  }
 }
-
-const Vegetables = class {
-    constructor(name) {
-        this.name = name
-    }
+class Vegetables {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
-printName = product=> console.log(product.name)
+printName = (product) => console.log(product.name);
 
-const cips = new FastFood("cips")
-const Migros = new Market("Migros", "OnderStreet")
-const armagan = new Customer("Armagan", "LiderStreet")
-const orderx = armagan.order(cips, Migros)
-const ayva = new Vegetables("ayva")
-const mert = new Customer("Mert","OnderStreet")
-const ordery = mert.order(ayva,Migros)
-console.log(armagan)
-Migros.printOrder()
+const chips = new FastFood("chips");
+const Migros = new Market("Migros", "OnderStreet");
+const Carrefour = new Market("Carrefour", "AtaturkStreet");
+const armagan = new Customer("Armagan", "LiderStreet");
+const order_Armagan = armagan.order(chips, Migros);
+const tomato = new Vegetables("tomato");
+const mert = new Customer("Mert", "OnderStreet");
+const order_Mert = mert.order(tomato, Carrefour);
+const rabia = new Customer("Rabia", "BagdatStreet");
+const patato = new Vegetables("patato");
+const order_Rabia = rabia.order(patato, Carrefour);
+console.log(armagan);
+Migros.printOrder();
+Carrefour.printOrder();
