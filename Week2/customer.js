8a7b6097b9552f3644707e749280e9c3243dc85c
rabiaaydmr <rabiaaydemir@sabanciuniv.edu>
2020-07-30 11:06:32 +0300

    Fix version hw2

--- Week2/customer.js
+++ Week2/customer.js
@@ -3,14 +3,6 @@ class Customer {
   constructor(name, address) {
     this.name = name;
     this.address = address;
-    this.customerorder = [];
-  }
-
-  order(product, market) {
-    //this.product=product.name
-    market.prepare(this.name, product);
-    this.customerorder.push(product);
-    //market.order.push(product)
   }
 }
 
--- Week2/products.js
+++ Week2/products.js
@@ -1,5 +1,4 @@
 const chalk = require("./node_modules/chalk");
-const Customer = require("./customer");
 
 module.exports = class Product {
   constructor(name) {
