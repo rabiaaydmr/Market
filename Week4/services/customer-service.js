1b335b504de9140e1bc1d73877a37e6ba5ae2b4d
rabiaaydmr <rabiaaydemir@sabanciuniv.edu>
2020-08-04 15:52:49 +0300

    Hw4

--- Week4/services/customer-service.js
+++ Week4/services/customer-service.js
@@ -1,11 +1,10 @@
-
-const BaseService = require('./base-service')
-const CustomerModel = require('../models/customer')
+const BaseService = require("./base-service");
+const Customer = require("../models/customer");
 
 class CustomerService extends BaseService {
-    constructor() {
-        super(CustomerModel, `${__dirname}/../customer-database.json`)
-    }
+  constructor() {
+    super(Customer, `${__dirname}/../customer-database.json`);
+  }
 }
 
-module.exports = new CustomerService()
+module.exports = new CustomerService();
