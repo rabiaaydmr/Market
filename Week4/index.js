2e3613d8daf2cf9f06af0d23f302e442f13ba9d3
rabiaaydmr <rabiaaydemir@sabanciuniv.edu>
2020-08-05 15:14:00 +0300

    Fixed according to cooments

--- Week4/index.js
+++ Week4/index.js
@@ -12,37 +12,32 @@ app.set("view engine", "pug");
 // Implementation for express framework, first parametre is the name of the path that we are gone respond to
 // and the second parametre is the callback function to tell our web server about how to respond to that :)
 app.get("/", (req, res) => {
-  //res.send('Hello')   // Now, my server knows that whenever it see ('/') then it reply this with ('Hello')
-
-  //res.sendFile(__dirname +'/index.html')
+  // res.send('Hello')   // Now, my server knows that whenever it see ('/') then it reply this with ('Hello')
+  // res.sendFile(__dirname +'/index.html')
   res.render("index");
 });
 
 app.get("/customer/all", async (req, res) => {
-  const person = await customerService.findAll();
-  // res.send(customer)  //almost like console.log(), it is sending to the browser
-  //it render person.pug  customer->array person->eachone
-  res.render("person", { customer: person });
+  const user = await customerService.findAll();
+  // res.send(customer)  // almost like console.log(), it is sending to the browser
+  // it renders customer.pug  customer->array person->eachone
+  res.render("customer", { customer: user });
 });
 
 app.get("/customer/:id", async (req, res) => {
   const id = req.params.id;
-  const person = await customerService.find(id);
-  res.send(person);
+  const user = await customerService.find(id);
+  res.send(user);
 });
+
 app.post("/customer", async (req, res) => {
-  console.log(req.body);
-  const person = await customerService.add(req.body);
-  res.send(person);
+  const user = await customerService.add(req.body);
+  res.send(user);
 });
 
 app.delete("/customer/:id", async (req, res) => {
   const user = await customerService.del(req.params.id);
   res.send(user);
-});
-
-app.listen(3000, () => {
-  console.log("Server listening");
 });
 
 app.get("/market/all", async (req, res) => {
@@ -52,37 +47,39 @@ app.get("/market/all", async (req, res) => {
 
 app.get("/market/:id", async (req, res) => {
   const id = req.params.id;
-  const market = await marketService.find(id);
-  res.send(market);
+  const markets = await marketService.find(id);
+  res.send(markets);
 });
 app.post("/market", async (req, res) => {
-  console.log(req.body);
-  const market = await marketService.add(req.body);
-  res.send(market);
+  const markets = await marketService.add(req.body);
+  res.send(markets);
 });
 
 app.delete("/market/:id", async (req, res) => {
-  const market = await marketService.del(req.params.id);
-  res.send(market);
+  const markets = await marketService.del(req.params.id);
+  res.send(markets);
 });
 
 app.get("/product/all", async (req, res) => {
-  const uniqueProduct = await productService.findAll();
-  res.render("product", { product: uniqueProduct });
+  const products = await productService.findAll();
+  res.render("product", { product: products });
 });
 
 app.get("/product/:id", async (req, res) => {
   const id = req.params.id;
-  const market = await productService.find(id);
-  res.send(product);
+  const products = await productService.find(id);
+  res.send(products);
 });
 app.post("/product", async (req, res) => {
-  console.log(req.body);
-  const product = await productService.add(req.body);
-  res.send(product);
+  const products = await productService.add(req.body);
+  res.send(products);
 });
 
 app.delete("/product/:id", async (req, res) => {
-  const product = await productService.del(req.params.id);
-  res.send(product);
+  const products = await productService.del(req.params.id);
+  res.send(products);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening");
 });
